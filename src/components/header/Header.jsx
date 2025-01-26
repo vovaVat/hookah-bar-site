ced4abe2c904616f3558365444151c7712d7ed79
Volodymyr <vovavat97@gmail.com>
2025-01-26 21:31:32 +0200

    hz

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Главная страница</li>
+          <li>Кальяны</li>
+          <li>Табаки</li>
+          <li>Еда</li>
+          <li>Напитки</li>
+          <li>О нас</li>
+          <li>Заведения</li>
+        </ul>
+        <div>
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <button>Book a table</button>
+      </nav>
+    </header>
+  );
+}
